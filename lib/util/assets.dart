a8cb995e70e6e821c3f3fb42c783538cab161852
shourya <shouryalala@gmail.com>
2021-01-18 11:51:54 +0530

    Text change to game based from gamification

--- lib/util/assets.dart
+++ lib/util/assets.dart
@@ -29,13 +29,13 @@ class Assets{
     'Safe',
   ];
 
-  static final String aboutUsDesc = 'Fello is a gamified savings and investment platform for users to save, grow and earn higher returns than a traditional savings bank account. For every ₹100 saved and invested through Fello, users get amazing rewards and incentives. \n\n'
+  static final String aboutUsDesc = 'Fello is a game based savings and investment platform for users to save, grow and earn higher returns than a traditional savings bank account. For every ₹100 saved and invested through Fello, users get amazing rewards and incentives. \n\n'
       'We (Manish & Shourya) are two finance folks who started Fello with the vision of helping people save money in a better way and learn about personal finance and investments with an added tinge of fun with games like never before. Now savings are no more boring, it is fun, safe and rewarding!\n\n'
       'We are an AMFI registered mutual fund distributor and all your money is invested directly into a relevant mutual fund. So the money saved is safe and secure with the security of a financial institution. \n\n'
       'If you have any queries, feel free to reach out to us and we would be happy to help you.';
 
   static final List<String> onboardingDesc = [
-    '${Constants.APP_NAME} is a gamified savings and investment platform. '
+    '${Constants.APP_NAME} is a game based savings and investment platform. '
         'We make saving a lot more rewarding, and investing a lot more simple.',
     '${Constants.APP_NAME} compiles carefully selected funds that are highly liquid and '
         'have historically given higher returns than traditional banks.',
@@ -68,7 +68,7 @@ class Assets{
   ];
 
   static final List<String> faqAnswers = [
-    'Fello is a gamified savings and investment platform for users to save, invest and grow their money '
+    'Fello is a game based savings and investment platform for users to save, invest and grow their money '
         'through SEBI/RBI regulated financial assets and earn more than their savings bank account returns. '
         'For every ₹100 invested through Fello, users get one tambola ticket using which they can participate '
         'in weekly Tambola draws and win amazing cash rewards and prizes.',
