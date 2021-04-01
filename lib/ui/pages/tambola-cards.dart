6b4d4b0855411d5820cdbfc5164ccd31c032b5bd
Arab Kumar <arabkumar1000@gmail.com>
2021-04-02 03:42:56 +0530

    tambola card scroll fix, transaction loading fix

--- lib/ui/pages/tambola-cards.dart
+++ lib/ui/pages/tambola-cards.dart
@@ -1,6 +1,8 @@
 import 'package:felloapp/base_util.dart';
 import 'package:felloapp/ui/elements/tambola_board_view.dart';
+import 'package:felloapp/util/ui_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TambolaCardsList extends StatelessWidget {
   final List<TambolaBoardView> tambolaBoardView;
@@ -9,7 +11,14 @@ class TambolaCardsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BaseUtil.getAppBar(),
+      appBar: AppBar(
+          backgroundColor: UiConstants.primaryColor,
+          iconTheme: IconThemeData(color: Colors.white),
+          title: Text(
+            "My Tambola Cards",
+            style: GoogleFonts.montserrat(
+                color: Colors.white, fontWeight: FontWeight.w500),
+          )),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 20),
         child: ListView.builder(
