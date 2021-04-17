c885e0f49376dc3f6ed571ff4f80dcccc4421d11
shourya <shouryalala@gmail.com>
2021-04-17 21:02:20 +0530

    added base object for ticket wallet

--- lib/core/model/UserTicketWallet.dart
+++ lib/core/model/UserTicketWallet.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:felloapp/base_util.dart';
 import 'package:felloapp/util/logger.dart';
 
-class UserTicketWallet{
+class UserTicketWallet {
   static Log log = new Log('UserTicketWallet');
 
   int _augGold99Tck;
@@ -15,22 +17,205 @@ class UserTicketWallet{
   static final String fldAugmontICICI1565TckCount = 'gICICI1565';
   static final String fldInitTckCount = 'gINIT';
   static final String fldPrizeRecurringTckCount = 'gPRIZE';
+  static final String fldReferralRecurringTckCount = 'gREF';
 
   static final String fldPrizeNonRecurringPrefix = 'gPRIZE';
   static final String fldReferralNonRecurringPrefix = 'gREF';
 
-  static final String fldReferralLockedTckCount = 'gREF_LOCK';
-
+  static final String fldReferralLockedPrefix = 'gLOCK';
 
   UserTicketWallet(this._augGold99Tck, this._icici1565Tck, this._initTck,
       this._prizeTck, this._refTck, this._activeNRTck, this._lockedTck);
 
+  UserTicketWallet.newTicketWallet()
+      : this(
+            null, null, null, BaseUtil.NEW_USER_TICKET_COUNT, null, null, null);
+
   UserTicketWallet.fromMap(Map<String, dynamic> tMap) {
-    // if(tMap.is)
+    if (tMap == null || tMap.isEmpty) {
+      this._initTck = BaseUtil.NEW_USER_TICKET_COUNT;
+      return;
+    }
+    if (_isValidField(tMap[fldAugmontGoldTckCount])) {
+      _augGold99Tck = tMap[fldAugmontGoldTckCount];
+    }
+    if (_isValidField(tMap[fldAugmontICICI1565TckCount])) {
+      _icici1565Tck = tMap[fldAugmontICICI1565TckCount];
+    }
+    if (_isValidField(tMap[fldInitTckCount])) {
+      _initTck = tMap[fldInitTckCount];
+    }
+    if (_isValidField(tMap[fldPrizeRecurringTckCount])) {
+      _prizeTck = tMap[fldPrizeRecurringTckCount];
+    }
+    if (_isValidField(tMap[fldReferralRecurringTckCount])) {
+      _refTck = tMap[fldReferralRecurringTckCount];
+    }
+    _buildNonRecurringTicketBalance(tMap);
+    _buildLockedTicketBalance(tMap);
+  }
+
+  ///all recurring tickets and all the non recurring tickets from this week
+  ///this DOESNT include the locked tickets
+  int getActiveTickets() {
+    int _baseCount = _augGold99Tck ??
+        0 + _icici1565Tck ??
+        0 + _prizeTck ??
+        0 + _initTck ??
+        0 + _refTck ??
+        0;
+    if(_activeNRTck != null && _activeNRTck.length > 0) {
+      for(NonRecurringTicket ticketCnt in _activeNRTck) {
+        _baseCount = _baseCount + ticketCnt.tckCount??0;
+      }
+    }
+
+    return _baseCount;
+  }
+
+  int getLockedTickets() {
+    int _baseCount = 0;
+    if(_lockedTck != null && _lockedTck.length > 0) {
+      for(LockedTicket ticketCnt in _lockedTck) {
+        _baseCount = _baseCount + ticketCnt.tckCount??0;
+      }
+    }
+
+    return _baseCount;
+  }
+
+  ///NON RECURRING TICKET FIELD FORMAT
+  /// gPREFIX_WEEKCODE_DAYCODE
+  /// where PREFIX = {PRIZE, REF}
+  /// WEEKCODE = weekcode for which ticket count is valid
+  /// DAYCODE = the day of the week from when the ticket is valid
+  _buildNonRecurringTicketBalance(Map<String, dynamic> tMap) {
+    _activeNRTck = [];
+    int _weekCde = _getWeekCode();
+    for (String key in tMap.keys) {
+      if (key.startsWith(fldReferralNonRecurringPrefix) ||
+          key.startsWith(fldPrizeNonRecurringPrefix)) {
+        //first break the parts
+        List<String> _strList = key.split('_');
+        if (_strList.length < 2) {
+          log.error('Invalid format for entry');
+          continue;
+        }
+        int _itemWeekCode, _itemDayCode;
+        //find the weekcode
+        try {
+          _itemWeekCode = int.parse(_strList[1]);
+        } catch (e) {
+          log.error('Week code parse failed');
+        }
+        //check whether weekcode is more than todays weekcode
+        if (_itemWeekCode == null || _itemWeekCode < _weekCde) {
+          //these are irrelevant
+          continue;
+        }
+        //see if the day code is available
+        if (_strList.length == 3) {
+          //this means the weekcode and daycode both are mentioned
+          try {
+            _itemDayCode = int.parse(_strList[2]);
+          } catch (e) {
+            log.error('Day code parse failed');
+          }
+        }
+        if (_itemWeekCode != null && _isValidField(tMap[key])) {
+          NonRecurringTicket _ticketCnt = NonRecurringTicket(
+              _strList[0], _itemWeekCode, _itemDayCode, tMap[key]);
+          _activeNRTck.add(_ticketCnt);
+        }
+      }
+    }
+  }
+
+  ///LOCKED TICKET FIELD FORMAT
+  /// gLOCK_SUFFIX
+  /// where SUFFIX = {REF}
+  _buildLockedTicketBalance(Map<String, dynamic> tMap) {
+    _lockedTck = [];
+    for (String key in tMap.keys) {
+      if (key.startsWith(fldReferralLockedPrefix)) {
+        //first break the parts
+        List<String> _strList = key.split('_');
+        if (_strList.length < 2) {
+          log.error('Invalid format for entry');
+          continue;
+        }
+        if (_isValidField(tMap[key])) {
+          LockedTicket _ticketCnt = LockedTicket(_strList[1], tMap[key]);
+          _lockedTck.add(_ticketCnt);
+        }
+      }
+    }
+  }
+
+  int _getWeekCode() {
+    DateTime td = DateTime.now();
+    Timestamp today = Timestamp.fromDate(td);
+    DateTime date = new DateTime.now();
+
+    return date.year * 100 + BaseUtil.getWeekNumber();
+  }
+
+  bool _isValidField(dynamic fld) {
+    if (fld == null) return false;
+    try {
+      int y = _cast<int>(fld);
+      if (y != null) return true;
+    } catch (e) {}
+
+    return false;
+  }
+
+  static T _cast<T>(x) => x is T ? x : null;
+
+  List<LockedTicket> get lockedTck => _lockedTck;
+
+  set lockedTck(List<LockedTicket> value) {
+    _lockedTck = value;
+  }
+
+  List<NonRecurringTicket> get activeNRTck => _activeNRTck;
+
+  set activeNRTck(List<NonRecurringTicket> value) {
+    _activeNRTck = value;
+  }
+
+  int get refTck => _refTck;
+
+  set refTck(int value) {
+    _refTck = value;
+  }
+
+  int get prizeTck => _prizeTck;
+
+  set prizeTck(int value) {
+    _prizeTck = value;
+  }
+
+  int get initTck => _initTck;
+
+  set initTck(int value) {
+    _initTck = value;
+  }
+
+  int get icici1565Tck => _icici1565Tck;
+
+  set icici1565Tck(int value) {
+    _icici1565Tck = value;
+  }
+
+  int get augGold99Tck => _augGold99Tck;
+
+  set augGold99Tck(int value) {
+    _augGold99Tck = value;
   }
 }
 
-class NonRecurringTicket{
+class NonRecurringTicket {
   final String type;
   final int weekCode;
   final int dayCode;
@@ -39,9 +224,9 @@ class NonRecurringTicket{
   NonRecurringTicket(this.type, this.weekCode, this.dayCode, this.tckCount);
 }
 
-class LockedTicket{
+class LockedTicket {
   final String type;
-  final String tckCount;
+  final int tckCount;
 
   LockedTicket(this.type, this.tckCount);
 }
