21aa0a10cba19b002a85d84eeb2b83cb6a122540
shourya <shouryalala@gmail.com>
2021-06-19 08:53:42 +0530

    claim prize http endpoint

--- lib/core/ops/http_ops.dart
+++ lib/core/ops/http_ops.dart
@@ -1,4 +1,6 @@
 import 'package:felloapp/base_util.dart';
+import 'package:felloapp/core/model/TambolaWinnersDetail.dart';
+import 'package:felloapp/core/model/UserFundWallet.dart';
 import 'package:felloapp/util/constants.dart';
 import 'package:felloapp/util/locator.dart';
 import 'package:felloapp/util/logger.dart';
@@ -14,35 +16,40 @@ class HttpModel extends ChangeNotifier {
   static const String _homeuri = 'https://fello-team.web.app';
   static const String _rzphomeuri =
       'https://us-central1-fello-d3a9c.cloudfunctions.net/razorpayops';
+  static const String _userTxnOpsUri =
+      'https://us-central1-fello-d3a9c.cloudfunctions.net/userTxnOps';
 
   ///Returns the number of tickets that need to be added to user's balance
-  Future<int> postUserReferral(String userId, String referee, String userName) async {
+  Future<int> postUserReferral(
+      String userId, String referee, String userName) async {
     if (_baseUtil == null || _baseUtil.firebaseUser == null) return -1;
     String idToken = await _baseUtil.firebaseUser.getIdToken();
     log.debug('Fetched user IDToken: ' + idToken);
     try {
-      http.Response _response = await http.post('$_homeuri/validateUserReferral?uid=$userId&rid=$referee&uname=$userName',
+      http.Response _response = await http.post(
+          '$_homeuri/validateUserReferral?uid=$userId&rid=$referee&uname=$userName',
           headers: {HttpHeaders.authorizationHeader: 'Bearer $idToken'});
       log.debug(_response.body);
-      if(_response.statusCode == 200) {
-        try{
+      if (_response.statusCode == 200) {
+        try {
           Map<String, dynamic> parsed = jsonDecode(_response.body);
-          if(parsed != null && parsed['add_tickets_count'] != null) {
+          if (parsed != null && parsed['add_tickets_count'] != null) {
             try {
               log.debug(parsed['add_tickets_count'].toString());
-              int userTicketUpdateCount = BaseUtil.toInt(parsed['add_tickets_count']);
+              int userTicketUpdateCount =
+                  BaseUtil.toInt(parsed['add_tickets_count']);
               return userTicketUpdateCount;
-            }catch(ee) {
+            } catch (ee) {
               return -1;
             }
-          }else {
+          } else {
             return -1;
           }
-        }catch(err) {
+        } catch (err) {
           log.error('Failed to parse ticket update count');
           return -1;
         }
-      }else {
+      } else {
         return -1;
       }
     } catch (e) {
@@ -62,7 +69,7 @@ class HttpModel extends ChangeNotifier {
     idToken = await _baseUtil.firebaseUser.getIdToken();
     log.debug('Fetched user IDToken: ' + idToken);
 
-    String amx = (amount*100).round().toString();
+    String amx = (amount * 100).round().toString();
     String _stage = Constants.activeRazorpayStage.value();
     String _uri = '$_rzphomeuri/$_stage/api/orderid?amount=$amx';
     if (notes != null) _uri = _uri + '&notes=${Uri.encodeComponent(notes)}';
@@ -107,6 +114,34 @@ class HttpModel extends ChangeNotifier {
     } catch (e) {
       log.error('Http post failed: ' + e.toString());
       return null;
+    }
+  }
+
+  Future<bool> registerPrizeClaim(
+      String userId, UserFundWallet fundWallet, double amount, PrizeClaimChoice claimChoice) async{
+    if (userId == null || amount == null || claimChoice == null) return null;
+    String _uri =
+        '$_userTxnOpsUri/api/registerPrizeClaim?userId=$userId&amount=$amount&redeemType=${claimChoice.value()}';
+    log.debug('URL: $_uri');
+
+    String idToken;
+    idToken = await _baseUtil.firebaseUser.getIdToken();
+    log.debug('Fetched user IDToken: ' + idToken);
+
+    try {
+      http.Response response = await http.get(_uri,
+          headers: {HttpHeaders.authorizationHeader: 'Bearer $idToken'});
+      log.debug(response.body);
+      Map<String, dynamic> parsed = jsonDecode(response.body);
+      if(response.statusCode == 200 && parsed['flag'] != null && parsed['flag'] == true) {
+        log.debug('Action successful');
+        return true;
+      }
+      //log.debug(parsed);
+      return false;
+    } catch (e) {
+      log.error('Http post failed: ' + e.toString());
+      return false;
     }
   }
 }
