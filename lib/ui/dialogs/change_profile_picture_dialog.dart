6192f00d23d75675054c3315fffcb4a2db13286f
shourya <shouryalala@gmail.com>
2021-08-26 23:14:31 +0530

    ios merge fix

--- lib/ui/dialogs/change_profile_picture_dialog.dart
+++ lib/ui/dialogs/change_profile_picture_dialog.dart
@@ -10,6 +10,7 @@ import 'package:felloapp/util/size_config.dart';
 import 'package:felloapp/util/ui_constants.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:path_provider/path_provider.dart';
