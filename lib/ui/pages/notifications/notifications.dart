811f608d34941ec3c680b78ce38adf8d0b8e79d9
Shreeyash Haritashya <haritashya@gmail.com>
2021-09-01 04:18:04 +0530

    feat: notifications UI completed

--- lib/ui/pages/notifications/notifications.dart
+++ lib/ui/pages/notifications/notifications.dart
@@ -1,4 +1,6 @@
 import 'package:felloapp/base_util.dart';
+import 'package:felloapp/util/size_config.dart';
+import 'package:felloapp/util/ui_constants.dart';
 import 'package:flutter/material.dart';
 
 class NotficationsPage extends StatelessWidget {
@@ -6,6 +8,25 @@ class NotficationsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BaseUtil.getAppBar(context, "Notifications"),
+      body: ListView.builder(
+        itemBuilder: (context, index) => Column(
+          children: [
+            ListTile(
+              title: Text(
+                'Notification Title',
+                style: TextStyle(
+                  color: UiConstants.textColor,
+                  fontSize: SizeConfig.screenHeight * 0.02,
+                ),
+              ),
+              subtitle: Text('Notification Subtitle'),
+              trailing: Text('4:00 PM'),
+            ),
+            Divider(),
+          ],
+        ),
+        itemCount: 2,
+      ),
     );
   }
 }
