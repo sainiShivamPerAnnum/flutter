df76d45a8725e2a4da045d94545415fcbe4faaf2
Arab Kumar <arabkumar1000@gmail.com>
2021-09-25 02:13:41 +0530

    added new dummy screens for fello 3.0

---
+++ lib/ui/pages/f3_dummy_pages/widgets.dart
@@ -0,0 +1,59 @@
+import 'package:felloapp/util/size_config.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Widgets {
+  getTitle(String text, Color color) => Text(
+        text,
+        style: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: color,
+            fontSize: SizeConfig.cardTitleTextSize),
+      );
+
+  getHeadlineLight(String text, Color color) => Text(
+        text,
+        style: TextStyle(
+          color: color,
+          fontSize: SizeConfig.largeTextSize,
+        ),
+      );
+
+  getHeadlineBold(String text, Color color) => Text(
+        text,
+        style: GoogleFonts.montserrat(
+            color: color,
+            fontWeight: FontWeight.w500,
+            fontSize: SizeConfig.largeTextSize),
+      );
+  getBodyLight(String text, Color color) => Text(
+        text,
+        style: TextStyle(
+          color: color,
+          fontSize: SizeConfig.mediumTextSize,
+        ),
+      );
+
+  getBodyBold(String text, Color color) => Text(
+        text,
+        style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.w500,
+            fontSize: SizeConfig.mediumTextSize),
+      );
+
+  getButton(String text, Function onPressed, Color color) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(primary: color),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: SizeConfig.mediumTextSize,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+}
