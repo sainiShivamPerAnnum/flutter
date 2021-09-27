6bafadc2739aa50368689479d07e79c68d32468e
Shreeyash Haritashya <haritashya@gmail.com>
2021-09-27 16:55:27 +0530

    feat: base view and viewmodel classes added

---
+++ lib/ui/architecture/base_viewmodel.dart
@@ -0,0 +1,16 @@
+//Project Imports
+import 'package:felloapp/core/enums/view_state.dart';
+
+//Flutter Imports
+import 'package:flutter/material.dart';
+
+class BaseModel extends ChangeNotifier {
+  ViewState _state = ViewState.Idle;
+
+  ViewState get state => _state;
+
+  void setState(ViewState viewState) {
+    _state = viewState;
+    notifyListeners();
+  }
+}
