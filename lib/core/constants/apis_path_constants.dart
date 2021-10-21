32b1e4aafe3d089bf93ddb5c5a9cd1c0210a95b0
Shreeyash Haritashya <haritashya@gmail.com>
2021-10-21 08:07:41 +0530

    feat: withdrawl apis call added to repo

--- lib/core/constants/apis_path_constants.dart
+++ lib/core/constants/apis_path_constants.dart
@@ -11,4 +11,6 @@ class ApiPath {
   get kDepositComplete => "/userFinanceOps/$stage/api/deposit/complete";
   get kDepositPending => "/userFinanceOps/$stage/api/deposit/pending";
   get kDepositCancelled => "/userFinanceOps/$stage/api/deposit/cancelled";
+  get kWithdrawlComplete => "/userFinanceOps/$stage/api/withdraw/complete";
+  get kWithdrawlCancelled => "/userFinanceOps/$stage/api/withdraw/cancelled";
 }
