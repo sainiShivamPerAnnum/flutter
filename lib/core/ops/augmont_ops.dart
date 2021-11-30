944313f515d633d5ce713a200a2f24a3e03a9505
Shreeyash Haritashya <haritashya@gmail.com>
2021-11-30 16:36:15 +0530

    chore: else added for false flag

--- lib/core/ops/augmont_ops.dart
+++ lib/core/ops/augmont_ops.dart
@@ -163,45 +163,6 @@ class AugmontModel extends ChangeNotifier {
       String mobile, String stateId) async {
     if (!isInit()) await _init();
 
-    // String _uid = _constructUid(mobile);
-    // String _uname = _constructUsername();
-    // var _params = {
-    //   CreateUser.fldMobile: mobile,
-    //   CreateUser.fldID: _uid,
-    //   CreateUser.fldUserName: _uname,
-    //   CreateUser.fldStateId: stateId,
-    // };
-
-    // var _request = http.Request(
-    //     'GET', Uri.parse(_constructRequest(CreateUser.path, _params)));
-    // _request.headers.addAll(headers);
-    // http.StreamedResponse _response = await _request.send();
-
-    // final resMap = await _processResponse(_response);
-    // if (resMap == null || !resMap[INTERNAL_FAIL_FLAG]) {
-    //   log.error('Query Failed');
-    //   return null;
-    // } else {
-    //   log.debug(resMap[CreateUser.resStatusCode].toString());
-    //   resMap["flag"] = QUERY_PASSED;
-
-    //   ///create augmont detail object
-    //   _baseProvider.augmontDetail =
-    //       UserAugmontDetail.newUser(_uid, _uname, stateId, '', '', '');
-    //   bool _a = false;
-
-    //   ///push the augmont detail object
-    //   _a = await _dbModel.updateUserAugmontDetails(
-    //       _baseProvider.myUser.uid, _baseProvider.augmontDetail);
-
-    //   ///switch augmont onboarding to true and notify listeners if everything goes in order
-    //   if (_a) {
-    //     _baseProvider.updateAugmontOnboarded(true);
-    //     await _dbModel.updateUser(_baseProvider.myUser);
-    //   }
-
-    //   return _baseProvider.augmontDetail;
-
     String uid = _userService.baseUser.uid;
     var data = {
       CreateUser.fldMobile: mobile,
@@ -224,6 +185,9 @@ class AugmontModel extends ChangeNotifier {
 
         _baseProvider.updateAugmontOnboarded(true);
         await _dbModel.updateUser(_baseProvider.myUser);
+      } else {
+        _logger.e('Create Simple user failed.');
+        return null;
       }
     } catch (e) {
       _logger.e('Query Failed $e');
