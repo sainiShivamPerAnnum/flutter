414ef7601884096af70e0cf2fc8b2595fe88f079
Shreeyash Haritashya <haritashya@gmail.com>
2021-12-02 16:30:35 +0530

    fix: weekcode fix added, take weekcode from last monday.

--- lib/util/code_from_freq.dart
+++ lib/util/code_from_freq.dart
@@ -1,14 +1,13 @@
-import 'package:felloapp/base_util.dart';
 import 'package:intl/intl.dart';
 
 class CodeFromFreq {
   static String getCodeFromFreq(String freq) {
-    final DateTime _currentTime = DateTime.now();
+    final DateTime _currentTime = getCorrectedMondayDate();
     final monthlyFormat = new DateFormat('yyyy-MM');
     String response = monthlyFormat.format(_currentTime);
 
     if (freq == 'weekly' || freq == 'daily') {
-      int weekcode = BaseUtil.getWeekNumber();
+      int weekcode = getWeekNumber(_currentTime);
       response += "-$weekcode";
       if (freq == 'daily') {
         final dailyFormat = new DateFormat('dd');
@@ -19,11 +18,30 @@ class CodeFromFreq {
   }
 
   static int getYearWeekCode() {
-    final DateTime _currentTime = DateTime.now();
+    final DateTime _currentTime = getCorrectedMondayDate();
     final int currentYear = _currentTime.year;
-    final int weekcode = BaseUtil.getWeekNumber();
+    final int weekcode = getWeekNumber(_currentTime);
 
     final response = currentYear * 100 + weekcode;
     return response;
+  }
+
+  // Get corrected Date
+  static DateTime getCorrectedMondayDate() {
+    final DateTime _currentDate = DateTime.now();
+    DateTime _correctedDate;
+    if (_currentDate.weekday > 1) {
+      _correctedDate = DateTime(_currentDate.year, _currentDate.month,
+          _currentDate.day - (_currentDate.weekday - 1));
+    } else {
+      _correctedDate = _currentDate;
+    }
+    return _correctedDate;
+  }
+
+  //Add getWeekNumber method here..
+  static int getWeekNumber(DateTime date) {
+    int dayOfYear = int.parse(DateFormat("D").format(date));
+    return ((dayOfYear - date.weekday + 10) / 7).floor();
   }
 }
