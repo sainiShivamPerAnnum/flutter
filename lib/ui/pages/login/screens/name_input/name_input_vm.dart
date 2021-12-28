e1464bd89ad6a5c7bf9b0aa8067877b4a12e7d00
Shreeyash <haritashya@gmail.com>
2021-12-28 14:20:37 +0530

    feat: login controller screens added to arch 3.0

---
+++ lib/ui/pages/login/screens/name_input/name_input_vm.dart
@@ -0,0 +1,5 @@
+import 'package:felloapp/ui/architecture/base_vm.dart';
+
+class NameInputScreenViewModel extends BaseModel {
+
+}
---
+++ lib/ui/pages/login/screens/username_input/username_input_vm.dart
@@ -0,0 +1,5 @@
+import 'package:felloapp/ui/architecture/base_vm.dart';
+
+class UsernameInputScreenViewModel extends BaseModel {
+
+}
