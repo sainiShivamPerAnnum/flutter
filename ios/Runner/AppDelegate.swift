2fdc79ea20b8d7b1f8a185de0f431bdfda9354e3
Shreeyash <haritashya@gmail.com>
2022-01-25 13:47:03 +0530

    fix: ios signup fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,9 @@
 import UIKit
 import Flutter
+import Firebase
+import FirebaseAuth
+import UserNotifications
+import FirebaseMessaging
 import WebEngage
 import webengage_flutter
 
@@ -17,4 +21,12 @@ import webengage_flutter
     WebEngage.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        let firebaseAuth = Auth.auth()
+        if (firebaseAuth.canHandleNotification(userInfo)){
+            print(userInfo)
+            return
+        }
+    }
+    
 }
