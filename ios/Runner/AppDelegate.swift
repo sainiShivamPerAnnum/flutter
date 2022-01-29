60c5916bf26bc22d8070ae56ff1b00a9039f1c26
Shreeyash <haritashya@gmail.com>
2022-01-29 17:21:43 +0530

    fix: ios data payload error fixed

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,6 @@
 import UIKit
 import Flutter
 import Firebase
-import FirebaseAuth
-import UserNotifications
 import WebEngage
 import webengage_flutter
 
@@ -11,20 +9,25 @@ import webengage_flutter
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         GeneratedPluginRegistrant.register(with: self)
-        WebEngage.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
-        return true
-    }
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let firebaseAuth = Auth.auth()
-        firebaseAuth.setAPNSToken(deviceToken, type: AuthAPNSTokenType.unknown)
-
-    }
-    override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        let firebaseAuth = Auth.auth()
-        if (firebaseAuth.canHandleNotification(userInfo)){
-            print(userInfo)
-            return
+        if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
         }
+        if(!UserDefaults.standard.bool(forKey: "Notification")) {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UserDefaults.standard.set(true, forKey: "Notification")
+        }
+        WebEngage.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-    
+
+    override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        super.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+          let firebaseAuth = Auth.auth()
+            Messaging.messaging().appDidReceiveMessage(userInfo)
+            print(userInfo)
+          if (firebaseAuth.canHandleNotification(userInfo)){
+              completionHandler(.noData)
+              return
+          }  b
+      }
 }
