56a68e490144309f739d3ea6ab241637334d4234
Shreeyash <haritashya@gmail.com>
2022-02-19 11:46:19 +0530

    feat: paytm flavours added

--- lib/core/service/paytm_service.dart
+++ lib/core/service/paytm_service.dart
@@ -1,16 +1,31 @@
+import 'package:felloapp/core/enums/page_state_enum.dart';
+import 'package:felloapp/util/credentials_stage.dart';
+import 'package:felloapp/util/flavor_config.dart';
 import 'package:flutter/services.dart';
 import 'package:paytm_allinonesdk/paytm_allinonesdk.dart';
 
 class PaytmService {
-  //Set mid and isStaging from flavor values.
-  String mid = "";
+  final String devMid = "qpHRfp13374268724583";
+  final String prodMid = "CMTNKX90967647249644";
+  String mid;
+  bool isStaging;
+
+  PaytmService() {
+    final stage = FlavorConfig.instance.values.paytmStage;
+    if (stage == PaytmStage.DEV) {
+      mid = devMid;
+      isStaging = true;
+    } else {
+      mid = prodMid;
+      isStaging = false;
+    }
+  }
 
   Future initiateTransactions(
       {String id,
       String orderId,
       String amount,
       String txnToken,
-      bool isStaging,
       bool restrictAppInvoke}) async {
     var result;
 
