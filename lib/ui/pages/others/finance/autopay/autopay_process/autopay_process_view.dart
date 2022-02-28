8e3dddd01e8d4e5d511eb006a2154b252137cb5b
Arab Kumar <arab@fello.in>
2022-02-28 11:55:25 +0530

    chore: minor changes to autopay process UI

--- lib/ui/pages/others/finance/autopay/autopay_process/autopay_process_view.dart
+++ lib/ui/pages/others/finance/autopay/autopay_process/autopay_process_view.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:felloapp/navigator/app_state.dart';
 import 'package:felloapp/ui/architecture/base_view.dart';
 import 'package:felloapp/ui/architecture/base_vm.dart';
 import 'package:felloapp/ui/pages/others/finance/autopay/autopay_process/autopay_process_vm.dart';
+import 'package:felloapp/ui/pages/others/games/tambola/tambola_home/tambola_home_view.dart';
 import 'package:felloapp/ui/pages/static/fello_appbar.dart';
 import 'package:felloapp/ui/pages/static/home_background.dart';
 import 'package:felloapp/ui/widgets/buttons/fello_button/large_button.dart';
@@ -9,6 +12,8 @@ import 'package:felloapp/util/styles/size_config.dart';
 import 'package:felloapp/util/styles/textStyles.dart';
 import 'package:felloapp/util/styles/ui_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
 class AutoPayProcessView extends StatefulWidget {
@@ -40,130 +45,38 @@ class _AutoPayProcessViewState extends State<AutoPayProcessView> {
               Column(
                 children: [
                   FelloAppBar(
-                    leading: FelloAppBarBackButton(),
+                    leading: model.processStatus == STATUS.Pending
+                        ? SizedBox()
+                        : FelloAppBarBackButton(),
                     title: "Set up AutoPay",
                   ),
                   Expanded(
                     child: Container(
-                      width: SizeConfig.screenWidth,
-                      padding: EdgeInsets.symmetric(
-                          horizontal: SizeConfig.pageHorizontalMargins),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(SizeConfig.padding40),
-                          topRight: Radius.circular(SizeConfig.padding40),
+                        width: SizeConfig.screenWidth,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: SizeConfig.pageHorizontalMargins),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(SizeConfig.padding40),
+                            topRight: Radius.circular(SizeConfig.padding40),
+                          ),
+                          color: Colors.white,
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.white,
+                              Colors.white,
+                              model.showSetAmountView
+                                  ? Colors.white
+                                  : model.gradColor,
+                            ],
+                          ),
                         ),
-                        color: Colors.white,
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            Colors.white,
-                            Colors.white,
-                            model.gradColor,
-                          ],
-                        ),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          if (model.processStatus == STATUS.Pending)
-                            pendingUI(model),
-                          if (model.processStatus == STATUS.Complete)
-                            completedUI(model),
-                          if (model.processStatus == STATUS.Cancel)
-                            cancelledUI(model),
-
-                          // Column(
-                          //   children: [
-                          //     SizedBox(height: SizeConfig.padding32),
-                          //     Text(
-                          //       "STEP 3/3",
-                          //       style: TextStyles.body2
-                          //           .colour(Colors.black26)
-                          //           .letterSpace(3),
-                          //     ),
-                          //     SizedBox(height: SizeConfig.padding8),
-                          //     Text(
-                          //       "How much would you like to save\neach day ?",
-                          //       style: TextStyles.title5.bold,
-                          //     ),
-                          //     SizedBox(height: SizeConfig.padding32),
-                          //     Row(
-                          //       mainAxisAlignment: MainAxisAlignment.center,
-                          //       children: [
-                          //         Text(
-                          //           "₹${sliderValue.toInt()}",
-                          //           style: GoogleFonts.sourceSansPro(
-                          //               fontWeight: FontWeight.bold,
-                          //               fontSize: SizeConfig.screenWidth / 4.8,
-                          //               color: Colors.black),
-                          //         ),
-                          //         Text(
-                          //           '/day',
-                          //           style: TextStyles.title2
-                          //               .colour(Colors.black38)
-                          //               .light
-                          //               .setHeight(5)
-                          //               .setHeight(4),
-                          //         )
-                          //       ],
-                          //     ),
-                          //     Slider(
-                          //       value: sliderValue,
-                          //       onChanged: (val) {
-                          //         updateSliderValue(val);
-                          //       },
-                          //       inactiveColor: UiConstants.scaffoldColor,
-                          //       thumbColor: UiConstants.primaryColor,
-                          //       activeColor:
-                          //           UiConstants.primaryColor.withOpacity(0.5),
-                          //       min: 10,
-                          //       max: 5000,
-                          //     ),
-                          //     Padding(
-                          //       padding: EdgeInsets.symmetric(
-                          //           horizontal: SizeConfig.pageHorizontalMargins),
-                          //       child: Row(
-                          //         children: [
-                          //           Text("₹10"),
-                          //           Spacer(),
-                          //           Text("₹5000"),
-                          //         ],
-                          //       ),
-                          //     )
-                          //   ],
-                          // )
-                        ],
-                      ),
-                    ),
+                        child: ui(model)),
                   ),
                 ],
               ),
-              // Align(
-              //   alignment: Alignment.bottomCenter,
-              //   child: SafeArea(
-              // child: Container(
-              //   margin: EdgeInsets.symmetric(
-              //       vertical: Platform.isIOS
-              //           ? SizeConfig.padding8
-              //           : SizeConfig.pageHorizontalMargins,
-              //       horizontal: SizeConfig.pageHorizontalMargins),
-              //   child: FelloButtonLg(
-              //     child: Text(
-              //       "Finish",
-              //       style: TextStyles.body2.bold.colour(Colors.white),
-              //     ),
-              //     onPressed: () {
-              //       AppState.backButtonDispatcher.didPopRoute();
-              //       // AppState.delegate.appState.currentAction = PageAction(
-              //       //     state: PageState.addPage,
-              //       //     page: AutoPayProcessViewPageConfig);
-              //     },
-              //   ),
-              // ),
-              //   ),
-              // )
             ],
           ),
         ),
@@ -171,8 +84,22 @@ class _AutoPayProcessViewState extends State<AutoPayProcessView> {
     });
   }
 
+  ui(AutoPayProcessViewModel model) {
+    if (model.processStatus == STATUS.Pending && !model.showSetAmountView)
+      return pendingUI(model);
+    else if (model.processStatus == STATUS.Complete && !model.showSetAmountView)
+      return completedUI(model);
+    else if (model.processStatus == STATUS.Cancel && !model.showSetAmountView)
+      return cancelledUI(model);
+    else if (model.showSetAmountView)
+      return amountSetUI(model);
+    else
+      return defaultUI(model);
+  }
+
   pendingUI(AutoPayProcessViewModel model) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Lottie.asset(
           "assets/lotties/pending.json",
@@ -195,10 +122,12 @@ class _AutoPayProcessViewState extends State<AutoPayProcessView> {
 
   completedUI(AutoPayProcessViewModel model) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Spacer(),
         Lottie.asset(
           "assets/lotties/complete.json",
-          height: SizeConfig.screenWidth / 2,
+          height: SizeConfig.screenWidth / 3,
         ),
         Text(
           "Yayy!",
@@ -212,21 +141,23 @@ class _AutoPayProcessViewState extends State<AutoPayProcessView> {
           textAlign: TextAlign.center,
         ),
         SizedBox(height: SizeConfig.padding24),
-        Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: SizeConfig.pageHorizontalMargins),
-          child: FelloButtonLg(
-            child: Text(
-              "Set Amount",
-              style: TextStyles.body2.bold.colour(Colors.white),
+        Spacer(),
+        SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              vertical: Platform.isIOS
+                  ? SizeConfig.padding8
+                  : SizeConfig.pageHorizontalMargins,
             ),
-            onPressed: () {
-              AppState.backButtonDispatcher.didPopRoute();
-
-              // AppState.delegate.appState.currentAction = PageAction(
-              //     state: PageState.addPage,
-              //     page: AutoPayProcessViewPageConfig);
-            },
+            child: FelloButtonLg(
+              child: Text(
+                "Set Amount",
+                style: TextStyles.body2.bold.colour(Colors.white),
+              ),
+              onPressed: () {
+                model.showSetAmountView = true;
+              },
+            ),
           ),
         ),
       ],
@@ -235,10 +166,13 @@ class _AutoPayProcessViewState extends State<AutoPayProcessView> {
 
   cancelledUI(AutoPayProcessViewModel model) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Spacer(),
         Lottie.asset(
           "assets/lotties/cancel.json",
-          height: SizeConfig.screenWidth / 2,
+          repeat: false,
+          height: SizeConfig.screenWidth / 3,
         ),
         Text(
           "Oh no!",
@@ -252,24 +186,118 @@ class _AutoPayProcessViewState extends State<AutoPayProcessView> {
           textAlign: TextAlign.center,
         ),
         SizedBox(height: SizeConfig.padding24),
-        Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: SizeConfig.pageHorizontalMargins),
-          child: FelloButtonLg(
-            child: Text(
-              "Try Again",
-              style: TextStyles.body2.bold.colour(Colors.white),
+        Spacer(),
+        SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              vertical: Platform.isIOS
+                  ? SizeConfig.padding8
+                  : SizeConfig.pageHorizontalMargins,
             ),
-            onPressed: () {
-              AppState.backButtonDispatcher.didPopRoute();
-
-              // AppState.delegate.appState.currentAction = PageAction(
-              //     state: PageState.addPage,
-              //     page: AutoPayProcessViewPageConfig);
-            },
+            child: FelloButtonLg(
+              child: Text(
+                "Try Again",
+                style: TextStyles.body2.bold.colour(Colors.white),
+              ),
+              onPressed: () {
+                model.tryAgain();
+                // AppState.delegate.appState.currentAction = PageAction(
+                //     state: PageState.addPage,
+                //     page: AutoPayProcessViewPageConfig);
+              },
+            ),
           ),
         ),
       ],
     );
+  }
+
+  amountSetUI(AutoPayProcessViewModel model) {
+    return Column(
+      children: [
+        SizedBox(height: SizeConfig.screenHeight * 0.1),
+        Text(
+          "STEP 3/3",
+          style: TextStyles.body2.colour(Colors.black26).letterSpace(3),
+        ),
+        SizedBox(height: SizeConfig.padding24),
+        SvgPicture.asset("assets/vectors/addmoney.svg",
+            height: SizeConfig.screenHeight * 0.16),
+        SizedBox(height: SizeConfig.padding24),
+        Text(
+          "How much would you like to save each day ?",
+          style: TextStyles.title5.bold,
+        ),
+        SizedBox(height: SizeConfig.padding32),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "₹${sliderValue.toInt()}",
+              style: GoogleFonts.sourceSansPro(
+                  fontWeight: FontWeight.bold,
+                  fontSize: SizeConfig.screenWidth / 4.8,
+                  color: Colors.black),
+            ),
+            Text(
+              '/day',
+              style: TextStyles.title2
+                  .colour(Colors.black38)
+                  .light
+                  .setHeight(5)
+                  .setHeight(4),
+            )
+          ],
+        ),
+        Slider(
+          value: sliderValue,
+          onChanged: (val) {
+            updateSliderValue(val);
+          },
+          inactiveColor: UiConstants.scaffoldColor,
+          thumbColor: UiConstants.primaryColor,
+          activeColor: UiConstants.primaryColor.withOpacity(0.5),
+          min: 10,
+          max: 5000,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: SizeConfig.pageHorizontalMargins),
+          child: Row(
+            children: [
+              Text("₹10"),
+              Spacer(),
+              Text("₹5000"),
+            ],
+          ),
+        ),
+        Spacer(),
+        SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              vertical: Platform.isIOS
+                  ? SizeConfig.padding8
+                  : SizeConfig.pageHorizontalMargins,
+            ),
+            child: FelloButtonLg(
+              child: Text(
+                "Finish",
+                style: TextStyles.body2.bold.colour(Colors.white),
+              ),
+              onPressed: () {
+                AppState.backButtonDispatcher.didPopRoute();
+                // AppState.delegate.appState.currentAction = PageAction(
+                //     state: PageState.addPage,
+                //     page: AutoPayProcessViewPageConfig);
+              },
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  defaultUI(AutoPayProcessViewModel model) {
+    return ListLoader();
   }
 }
--- lib/ui/pages/others/finance/autopay/autopay_process/autopay_process_vm.dart
+++ lib/ui/pages/others/finance/autopay/autopay_process/autopay_process_vm.dart
@@ -9,6 +9,14 @@ enum STATUS { Pending, Complete, Cancel }
 class AutoPayProcessViewModel extends BaseModel {
   STATUS _processStatus = STATUS.Pending;
   Color _gradColor = UiConstants.tertiaryLight;
+  bool _showSetAmountView = false;
+
+  bool get showSetAmountView => this._showSetAmountView;
+
+  set showSetAmountView(showSetAmountView) {
+    this._showSetAmountView = showSetAmountView;
+    notifyListeners();
+  }
 
   get gradColor => this._gradColor;
 
@@ -27,9 +35,21 @@ class AutoPayProcessViewModel extends BaseModel {
   init() async {
     Future.delayed(Duration(seconds: 2), () {
       Random r = new Random();
-      bool result = r.nextDouble() <= 0.7;
-      processStatus = result ? STATUS.Complete : STATUS.Cancel;
-      gradColor = result ? UiConstants.primaryLight : Color(0xffEF6D6D);
+      bool result = true;
+      //r.nextDouble() <= 0.7;
+      processStatus =
+          // result ? STATUS.Complete :
+          STATUS.Cancel;
+      gradColor = Colors.white;
+      // result ? UiConstants.primaryLight : Colors.white;
+    });
+  }
+
+  tryAgain() {
+    processStatus = STATUS.Pending;
+    Future.delayed(Duration(seconds: 2), () {
+      processStatus = STATUS.Complete;
+      gradColor = Colors.white;
     });
   }
 }
