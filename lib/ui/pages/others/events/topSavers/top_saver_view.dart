d8b1bad2e8df821e1bc30abcfc7372a779bd958a
Devesh Tiwary <deveshtiwary19@gmail.com>
2022-08-08 17:56:16 +0530

    chore: Fix

--- lib/ui/pages/others/events/topSavers/top_saver_view.dart
+++ lib/ui/pages/others/events/topSavers/top_saver_view.dart
@@ -530,17 +530,9 @@ class EventLeaderboard extends StatelessWidget {
                     style: TextStyles.title5.bold,
                   ),
                   Spacer(),
-                  GestureDetector(
-                    onTap: () {
-                      AppState.delegate.appState.currentAction = PageAction(
-                          page: CampaignViewPageConfig,
-                          state: PageState.addWidget,
-                          widget: NotficationsPage());
-                    },
-                    child: Text(
-                      "Your rank: ${model.userRank == 0 ? '-' : model.userRank.toString().padLeft(2, '0')} ",
-                      style: TextStyles.body3,
-                    ),
+                  Text(
+                    "Your rank: ${model.userRank == 0 ? '-' : model.userRank.toString().padLeft(2, '0')} ",
+                    style: TextStyles.body3,
                   )
                 ],
               ),
