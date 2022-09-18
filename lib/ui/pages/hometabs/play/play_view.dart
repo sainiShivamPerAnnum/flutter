a43ab7e9348a3efcdde6746cf988ff7198f1b124
Arab Kumar <arabkumar@Arabs-MacBook-Pro.local>
2022-09-18 11:25:48 +0530

    import: added play_view import

--- lib/ui/pages/hometabs/play/play_view.dart
+++ lib/ui/pages/hometabs/play/play_view.dart
@@ -1,24 +1,15 @@
 import 'dart:developer';
 
-import 'package:felloapp/core/enums/page_state_enum.dart';
-import 'package:felloapp/navigator/app_state.dart';
-import 'package:felloapp/navigator/router/ui_pages.dart';
 import 'package:felloapp/ui/architecture/base_view.dart';
 import 'package:felloapp/ui/pages/hometabs/play/play_components/gow_card.dart';
 import 'package:felloapp/ui/pages/hometabs/play/play_components/play_info_section.dart';
 import 'package:felloapp/ui/pages/hometabs/play/play_components/play_title.dart';
 import 'package:felloapp/ui/pages/hometabs/play/play_components/trendingGames.dart';
 import 'package:felloapp/ui/pages/hometabs/play/play_viewModel.dart';
-import 'package:felloapp/ui/pages/hometabs/save/save_view.dart';
-import 'package:felloapp/ui/pages/root/root_vm.dart';
-import 'package:felloapp/ui/service_elements/user_service/profile_image.dart';
-import 'package:felloapp/ui/widgets/coin_bar/coin_bar_view.dart';
-import 'package:felloapp/util/assets.dart';
-import 'package:felloapp/util/locator.dart';
 import 'package:felloapp/util/styles/size_config.dart';
-import 'package:felloapp/util/styles/textStyles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../widgets/appbar/appbar.dart';
 import 'play_components/more_games_section.dart';
 
 class Play extends StatelessWidget {
