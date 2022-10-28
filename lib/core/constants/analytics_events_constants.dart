9ebc5055ee3bad23373d58dd4f20c4161efc10e0
Devesh Tiwary <deveshtiwary19@gmail.com>
2022-10-28 14:54:48 +0530

    chore: Added analytics phase 13

--- lib/core/constants/analytics_events_constants.dart
+++ lib/core/constants/analytics_events_constants.dart
@@ -174,4 +174,5 @@ class AnalyticsEvents {
   static const redeemWinningsTapped = "Redeem Winnings Tapped";
   static const questionMarkTaoped = "Question Mark Tapped";
   static const goldenTicketSectionOpen = 'Golden Ticket section open';
+  static const journeyMileStarted = 'Journey Milestone Started';
 }
