a7daea9dfba29f7b4a883be534db75ea72e3e616
Harsha <konndapuramharsha@gmail.com>
2022-11-11 12:05:41 +0530

    chore: updated null values in verify pan, amount  models

--- lib/core/model/verify_pan_response_model.dart
+++ lib/core/model/verify_pan_response_model.dart
@@ -1,8 +1,8 @@
 class VerifyPanResponseModel {
   bool? flag;
-  String? upstreamName;
-  String? message;
-  String? gtId;
+  String ? upstreamName;
+  String ? message;
+  String ? gtId;
 
   VerifyPanResponseModel(
       {this.flag, this.upstreamName, this.message, this.gtId});
