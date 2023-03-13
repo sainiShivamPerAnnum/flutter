5081e7f285c035cb319827070a470aec57041923
Arab Kumar <arabkumar@Arabs-MacBook-Pro.local>
2023-03-13 16:17:44 +0530

    chore: fixing gitIgnore

--- ios/Pods/Target Support Files/flutter_dynamic_icon/flutter_dynamic_icon-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_dynamic_icon : NSObject
-@end
-@implementation PodsDummy_flutter_dynamic_icon
-@end
--- ios/Pods/Target Support Files/sms_autofill/sms_autofill-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_sms_autofill : NSObject
-@end
-@implementation PodsDummy_sms_autofill
-@end
