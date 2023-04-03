e565c3bffc901c3a0e4c1562bdbac466ea0484f0
ajaygujja <ajaygujja5@gmail.com>
2023-04-03 19:11:06 +0530

    chore:  added getMatches Model

--- lib/core/repository/power_play_repo.dart
+++ lib/core/repository/power_play_repo.dart
@@ -1,7 +1,9 @@
 import 'package:felloapp/core/repository/base_repo.dart';
+import 'package:felloapp/core/service/cache_service.dart';
 import 'package:felloapp/util/flavor_config.dart';
 
 class PowerPlayRepository extends BaseRepo {
+  final _cacheService = CacheService();
   final String _baseUrl = FlavorConfig.isDevelopment()
       ? "https://rco4comkpa.execute-api.ap-south-1.amazonaws.com/dev"
       : "https://l4aighxmj3.execute-api.ap-south-1.amazonaws.com/prod";
