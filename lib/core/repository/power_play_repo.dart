20d12bade0ffb0120976f91f58d8078d95569e09
Arab Kumar <arabkumar@Arabs-MacBook-Pro.local>
2023-04-03 17:12:26 +0530

    chore: added repository and service for powerplay

---
+++ lib/core/repository/power_play_repo.dart
@@ -0,0 +1,8 @@
+import 'package:felloapp/core/repository/base_repo.dart';
+import 'package:felloapp/util/flavor_config.dart';
+
+class PowerPlayRepository extends BaseRepo {
+  final String _baseUrl = FlavorConfig.isDevelopment()
+      ? "https://rco4comkpa.execute-api.ap-south-1.amazonaws.com/dev"
+      : "https://l4aighxmj3.execute-api.ap-south-1.amazonaws.com/prod";
+}
