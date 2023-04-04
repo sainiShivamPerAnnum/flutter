9bca0a8c3b12929ff3d760574dead58427639bae
Arab Kumar <arabkumar@Arabs-MacBook-Pro.local>
2023-04-04 13:00:01 +0530

    chore: updated completed match details view

--- lib/ui/pages/power_play/completed_match_details/completed_match_details_view.dart
+++ lib/ui/pages/power_play/completed_match_details/completed_match_details_view.dart
@@ -2,6 +2,7 @@ import 'package:felloapp/ui/elements/appbar/appbar.dart';
 import 'package:felloapp/ui/pages/power_play/leaderboard/prediction_leaderboard_view.dart';
 import 'package:felloapp/ui/pages/power_play/shared_widgets/power_play_bg.dart';
 import 'package:felloapp/util/assets.dart';
+import 'package:felloapp/util/haptic.dart';
 import 'package:felloapp/util/styles/size_config.dart';
 import 'package:felloapp/util/styles/textStyles.dart';
 import 'package:felloapp/util/styles/ui_constants.dart';
@@ -14,152 +15,186 @@ class CompletedMatchDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PowerPlayBackgroundUi(
-      child: SizedBox(
-        width: SizeConfig.screenWidth,
-        height: SizeConfig.screenHeight,
-        child: Column(
-          children: [
-            const FAppBar(
-              showAvatar: false,
-              showCoinBar: false,
-            ),
-            SvgPicture.network(
-              Assets.powerPlayMain,
-              height: SizeConfig.screenWidth! * 0.2,
-            ),
-            const PowerPlayTotalWinWidget(),
-            const WinTextWidget(),
-            const CustomDivider(),
-            MatchBriefDetailsWidget(
-              padding: EdgeInsets.symmetric(horizontal: SizeConfig.padding40),
-            ),
-            const UserPredictionsButton(),
-            Expanded(
-              child: Container(
-                width: SizeConfig.screenWidth,
-                margin: EdgeInsets.only(
-                  top: SizeConfig.pageHorizontalMargins,
-                  right: SizeConfig.pageHorizontalMargins,
-                  left: SizeConfig.pageHorizontalMargins,
+      child: Stack(
+        children: [
+          SizedBox(
+            width: SizeConfig.screenWidth,
+            height: SizeConfig.screenHeight,
+            child: Column(
+              children: [
+                SizedBox(
+                    height: SizeConfig.viewInsets.top + kToolbarHeight / 2),
+                SvgPicture.network(
+                  Assets.powerPlayMain,
+                  height: SizeConfig.screenWidth! * 0.2,
                 ),
-                child: Column(children: [
-                  Text(
-                    "Correct Predictors of the Match",
-                    style: TextStyles.sourceSansB.title5,
-                  ),
-                  SizedBox(height: SizeConfig.padding16),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: SizeConfig.screenWidth! * 0.13,
-                        child: Text(
-                          "#Rank",
-                          style: TextStyles.sourceSans.body3
-                              .colour(Colors.white38),
-                        ),
-                      ),
-                      Text(
-                        "Username",
-                        style:
-                            TextStyles.sourceSans.body3.colour(Colors.white38),
-                      ),
-                      const Spacer(),
-                      Text(
-                        "Prediction",
-                        style:
-                            TextStyles.sourceSans.body3.colour(Colors.white38),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: ListView.separated(
-                      padding: EdgeInsets.zero,
-                      itemCount: 4,
-                      itemBuilder: (ctx, i) => Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: SizeConfig.padding10),
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: SizeConfig.screenWidth! * 0.13,
-                              child: Text(
-                                "#$i",
-                                style: TextStyles.sourceSansSB.body1,
-                              ),
-                            ),
-                            Expanded(
-                                child: Row(
-                              children: [
-                                CircleAvatar(
-                                  radius: SizeConfig.iconSize1,
-                                  backgroundColor: Colors.black,
-                                ),
-                                SizedBox(width: SizeConfig.padding10),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "@qwertyuiop",
-                                        style: TextStyles.sourceSans.body3,
-                                      ),
-                                      SizedBox(height: SizeConfig.padding2),
-                                      Text(
-                                        "won ₹120 worth of digital gold",
-                                        style: TextStyles.sourceSans.body3,
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            )),
-                            SizedBox(width: SizeConfig.padding10),
-                            Text(
-                              "₹120 | 5:56 PM",
-                              style: TextStyles.sourceSans.body3,
-                            )
-                          ],
-                        ),
-                      ),
-                      separatorBuilder: (ctx, i) => (i == 3)
-                          ? const SizedBox()
-                          : const Divider(
-                              color: Colors.white30,
-                              thickness: 0.3,
-                            ),
-                    ),
-                  ),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "View All Winners",
-                        style: TextStyles.sourceSans.body2.underline,
-                      ))
-                ]),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  left: SizeConfig.pageHorizontalMargins,
-                  right: SizeConfig.pageHorizontalMargins,
-                  bottom: SizeConfig.pageHorizontalMargins),
-              child: MaterialButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                color: Colors.white,
-                onPressed: () {},
-                child: Center(
-                  child: Text(
-                    'INVITE FRIENDS',
-                    style: TextStyles.rajdhaniB.body1.colour(Colors.black),
+                const PowerPlayTotalWinWidget(),
+                // const WinTextWidget(),
+                const LossOrNoParticipateTextWidget(),
+                const CustomDivider(),
+                MatchBriefDetailsWidget(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: SizeConfig.padding40,
                   ),
                 ),
+                const UserPredictionsButton(),
+                const CorrectPredictorsListView(),
+                const FooterCta(),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              height: SizeConfig.fToolBarHeight,
+              child: const FAppBar(
+                showAvatar: false,
+                showCoinBar: false,
               ),
             ),
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FooterCta extends StatelessWidget {
+  const FooterCta({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+          left: SizeConfig.pageHorizontalMargins,
+          right: SizeConfig.pageHorizontalMargins,
+          bottom: SizeConfig.pageHorizontalMargins),
+      child: MaterialButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        color: Colors.white,
+        onPressed: () {
+          Haptic.vibrate();
+        },
+        child: Center(
+          child: Text(
+            'INVITE FRIENDS',
+            style: TextStyles.rajdhaniB.body1.colour(Colors.black),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class CorrectPredictorsListView extends StatelessWidget {
+  const CorrectPredictorsListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        width: SizeConfig.screenWidth,
+        margin: EdgeInsets.only(
+          top: SizeConfig.pageHorizontalMargins,
+          right: SizeConfig.pageHorizontalMargins,
+          left: SizeConfig.pageHorizontalMargins,
+        ),
+        child: Column(children: [
+          Text(
+            "Correct Predictors of the Match",
+            style: TextStyles.sourceSansB.title5,
+          ),
+          SizedBox(height: SizeConfig.padding16),
+          Row(
+            children: [
+              SizedBox(
+                width: SizeConfig.screenWidth! * 0.13,
+                child: Text(
+                  "#Rank",
+                  style: TextStyles.sourceSans.body3.colour(Colors.white38),
+                ),
+              ),
+              Text(
+                "Username",
+                style: TextStyles.sourceSans.body3.colour(Colors.white38),
+              ),
+              const Spacer(),
+              Text(
+                "Prediction",
+                style: TextStyles.sourceSans.body3.colour(Colors.white38),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.separated(
+              padding: EdgeInsets.zero,
+              itemCount: 4,
+              itemBuilder: (ctx, i) => Padding(
+                padding: EdgeInsets.symmetric(vertical: SizeConfig.padding10),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: SizeConfig.screenWidth! * 0.13,
+                      child: Text(
+                        "#$i",
+                        style: TextStyles.sourceSansSB.body1,
+                      ),
+                    ),
+                    Expanded(
+                        child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: SizeConfig.iconSize1,
+                          backgroundColor: Colors.black,
+                        ),
+                        SizedBox(width: SizeConfig.padding10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "@qwertyuiop",
+                                style: TextStyles.sourceSans.body3,
+                              ),
+                              SizedBox(height: SizeConfig.padding2),
+                              Text(
+                                "won ₹120 worth of digital gold",
+                                style: TextStyles.sourceSans.body4
+                                    .colour(Colors.white70),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
+                    SizedBox(width: SizeConfig.padding16),
+                    Text(
+                      "₹120 | 5:56 PM",
+                      style: TextStyles.sourceSans.body3,
+                    )
+                  ],
+                ),
+              ),
+              separatorBuilder: (ctx, i) => (i == 3)
+                  ? const SizedBox()
+                  : const Divider(
+                      color: Colors.white30,
+                      thickness: 0.3,
+                    ),
+            ),
+          ),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                "View All Winners",
+                style: TextStyles.sourceSans.body2.underline,
+              ))
+        ]),
       ),
     );
   }
@@ -248,6 +283,40 @@ class WinTextWidget extends StatelessWidget {
             textAlign: TextAlign.center,
             style:
                 TextStyles.sourceSansB.title5.colour(UiConstants.primaryColor),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class LossOrNoParticipateTextWidget extends StatelessWidget {
+  const LossOrNoParticipateTextWidget({super.key, this.isLoss = true});
+
+  final bool isLoss;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(
+        SizeConfig.pageHorizontalMargins,
+      ),
+      child: Column(
+        children: [
+          SvgPicture.asset(Assets.uhoh, height: SizeConfig.padding54),
+          SizedBox(height: SizeConfig.padding16),
+          Text(
+            isLoss
+                ? "Your Prediction for today wasn’t right :("
+                : "You did not predict in this match",
+            textAlign: TextAlign.center,
+            style: TextStyles.sourceSansB.body2,
+          ),
+          Text(
+            isLoss
+                ? "Try Again in the next match!"
+                : "Make predictions to win rewards!",
+            textAlign: TextAlign.center,
+            style: TextStyles.sourceSans.body3,
           )
         ],
       ),
