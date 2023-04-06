3282d171082c9a006bd04a2781bdbc9ace6b9e4e
ajaygujja <ajaygujja5@gmail.com>
2023-04-06 15:30:09 +0530

    chore: removed unwanted file

--- lib/ui/pages/power_play/shared_widgets/power_play_bg.dart
+++ lib/ui/pages/power_play/shared_widgets/power_play_bg.dart
@@ -3,12 +3,10 @@ import 'package:felloapp/util/styles/ui_constants.dart';
 import 'package:flutter/material.dart';
 
 class PowerPlayBackgroundUi extends StatelessWidget {
-  const PowerPlayBackgroundUi(
-      {Key? key, required this.child, this.floatingActionButton})
+  const PowerPlayBackgroundUi({Key? key, required this.child})
       : super(key: key);
 
   final Widget child;
-  final Widget? floatingActionButton;
 
   @override
   Widget build(BuildContext context) {
@@ -19,17 +17,16 @@ class PowerPlayBackgroundUi extends StatelessWidget {
           gradient: LinearGradient(
             colors: [
               UiConstants.kPowerPlayPrimary,
-              Color(0xff3C2840),
+              const Color(0xff3C2840),
               UiConstants.kPowerPlaySecondary,
             ],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            stops: [0.3, 0.6, 1],
+            stops: const [0.3, 0.6, 1],
           ),
         ),
         child: child,
       ),
-      floatingActionButton: floatingActionButton,
     );
   }
 }
