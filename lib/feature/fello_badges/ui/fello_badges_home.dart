4642e64636a615d534b0a5f2c6a46d754a15d6c7
ajaygujja <ajaygujja5@gmail.com>
2023-09-13 15:28:20 +0530

    chore: how superfello work widget added

--- lib/feature/fello_badges/ui/fello_badges_home.dart
+++ lib/feature/fello_badges/ui/fello_badges_home.dart
@@ -123,9 +123,201 @@ class _FelloBadgeHomeState extends State<FelloBadgeHome> {
               SizedBox(
                 height: SizeConfig.padding34,
               ),
+              HowSuperFelloWorksWidget(
+                isBoxOpen: false,
+                onStateChanged: () {
+                  // setState(() {});
+                },
+              ),
+              SizedBox(
+                height: SizeConfig.padding34,
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HowSuperFelloWorksWidget extends StatefulWidget {
+  const HowSuperFelloWorksWidget(
+      {super.key, required this.onStateChanged, this.isBoxOpen = true});
+
+  final Function onStateChanged;
+  final bool isBoxOpen;
+
+  @override
+  State<HowSuperFelloWorksWidget> createState() =>
+      _HowSuperFelloWorksWidgetState();
+}
+
+class _HowSuperFelloWorksWidgetState extends State<HowSuperFelloWorksWidget> {
+  bool isBoxOpen = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isBoxOpen = widget.isBoxOpen;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      width: SizeConfig.screenWidth,
+      margin: EdgeInsets.symmetric(
+        horizontal: SizeConfig.padding12,
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: SizeConfig.padding12,
+        vertical: SizeConfig.padding4,
+      ),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment(-0.01, 1.00),
+          end: Alignment(0.01, -1),
+          colors: [Color(0xFF3A393C), Color(0xFF232326)],
+        ),
+        borderRadius: BorderRadius.circular(SizeConfig.roundness12),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextButton(
+            onPressed: () {
+              setState(() {
+                isBoxOpen = !isBoxOpen;
+              });
+
+              // Future.delayed(const Duration(milliseconds: 200), () {
+              //   widget.onStateChanged();
+              // });
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(),
+                Container(
+                  margin: EdgeInsets.only(left: SizeConfig.padding36),
+                  child: Text(
+                    'How SuperFello works?',
+                    style: TextStyles.rajdhaniSB.body1,
+                  ),
+                ),
+                const Spacer(),
+                Icon(
+                  isBoxOpen
+                      ? Icons.keyboard_arrow_up
+                      : Icons.keyboard_arrow_down,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+          if (isBoxOpen)
+            SizedBox(
+              height: SizeConfig.padding20,
+            ),
+          isBoxOpen
+              ? AnimatedContainer(
+                  duration: const Duration(milliseconds: 1200),
+                  curve: Curves.easeIn,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Earn a badge by completing a simple task',
+                        textAlign: TextAlign.center,
+                        style: TextStyles.sourceSans.body2.colour(
+                          Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.padding8,
+                      ),
+                      SizedBox(
+                        height: 24,
+                        child: ListView.builder(
+                            padding: EdgeInsets.zero,
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: 10,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Container(
+                                    width: SizeConfig.padding1,
+                                    height: SizeConfig.padding4,
+                                    decoration: const BoxDecoration(
+                                      color: Color(0xffA5FCE7),
+                                      shape: BoxShape.rectangle,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: SizeConfig.padding4,
+                                  ),
+                                ],
+                              );
+                            }),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.padding8,
+                      ),
+                      Text(
+                        'You will auto collect that badge as the task is completed',
+                        textAlign: TextAlign.center,
+                        style: TextStyles.sourceSans.body2.colour(
+                          Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.padding8,
+                      ),
+                      SizedBox(
+                        height: 24,
+                        child: ListView.builder(
+                            padding: EdgeInsets.zero,
+                            itemCount: 10,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Container(
+                                    width: SizeConfig.padding1,
+                                    height: SizeConfig.padding4,
+                                    decoration: const BoxDecoration(
+                                      color: Color(0xffA5FCE7),
+                                      shape: BoxShape.rectangle,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: SizeConfig.padding4,
+                                  ),
+                                ],
+                              );
+                            }),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.padding8,
+                      ),
+                      Text(
+                        'Increase level to SUPER Fello when you collect all 9 super badges',
+                        textAlign: TextAlign.center,
+                        style: TextStyles.sourceSans.body2.colour(
+                          Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.padding16,
+                      ),
+                    ],
+                  ),
+                )
+              : const SizedBox()
+        ],
       ),
     );
   }
@@ -205,13 +397,13 @@ class FelloBadgeDetails extends StatelessWidget {
                               title[index],
                               style: TextStyles.rajdhaniB.title2
                                   .colour(
-                                index == 2
-                                    ? const Color(0xFFFFD979)
-                                    : Colors.white,
-                              )
+                                    index == 2
+                                        ? const Color(0xFFFFD979)
+                                        : Colors.white,
+                                  )
                                   .copyWith(
-                                height: 1.27,
-                              ),
+                                    height: 1.27,
+                                  ),
                             ),
                             SvgPicture.network(
                               imageUrl[index],
@@ -1132,15 +1324,6 @@ class FelloBadgeBGCustomPainter extends CustomPainter {
   }
 }
 
-// import 'dart:ui' as ui;
-//
-// //Add this CustomPaint widget to the Widget Tree
-// CustomPaint(
-// size: Size(WIDTH, (WIDTH*0.30666666666666664).toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-// painter: RPSCustomPainter(),
-// )
-
-//Copy this CustomPainter code to the Bottom of the File
 class FelloBadgeBG2CustomPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
@@ -1154,6 +1337,39 @@ class FelloBadgeBG2CustomPainter extends CustomPainter {
 
     Paint paint_0_fill = Paint()..style = PaintingStyle.fill;
     paint_0_fill.color = const Color(0xff023C40).withOpacity(1.0);
+    canvas.drawPath(path_0, paint_0_fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+// import 'dart:ui' as ui;
+//
+// //Add this CustomPaint widget to the Widget Tree
+// CustomPaint(
+// size: Size(2, 25),
+// painter: RPSCustomPainter(),
+// )
+
+//Copy this CustomPainter code to the Bottom of the File
+class RPSCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path_0 = Path();
+    path_0.moveTo(1, 0.980469);
+    path_0.lineTo(1, 24.9605);
+
+    Paint paint_0_stroke = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = size.width * 0.6483350;
+    paint_0_stroke.color = const Color(0xffA5FCE7).withOpacity(1.0);
+    canvas.drawPath(path_0, paint_0_stroke);
+
+    Paint paint_0_fill = Paint()..style = PaintingStyle.fill;
+    paint_0_fill.color = const Color(0xff000000).withOpacity(1.0);
     canvas.drawPath(path_0, paint_0_fill);
   }
 
