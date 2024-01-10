2b5bfb9b1a6285a8b04e8f55aff9986671405048
Hirdesh Garg <hirdeshgarg@Hirdeshs-MacBook-Pro.local>
2024-01-10 19:38:33 +0530

    feat: saving tabs older

--- lib/ui/pages/userProfile/my_winnings/my_winnings_view.dart
+++ lib/ui/pages/userProfile/my_winnings/my_winnings_view.dart
@@ -1,10 +1,10 @@
-import 'package:felloapp/core/enums/faqTypes.dart';
 import 'package:felloapp/core/service/notifier_services/scratch_card_service.dart';
 import 'package:felloapp/core/service/notifier_services/user_service.dart';
-import 'package:felloapp/navigator/app_state.dart';
 import 'package:felloapp/ui/architecture/base_view.dart';
-import 'package:felloapp/ui/pages/login/login_components/login_support.dart';
+import 'package:felloapp/ui/pages/rewards/earn_rewards/rewards_details.dart';
+import 'package:felloapp/ui/pages/rewards/earn_rewards/rewards_intro.dart';
 import 'package:felloapp/ui/pages/rewards/scratch_card/scratch_card_view.dart';
+import 'package:felloapp/ui/pages/static/loader_widget.dart';
 import 'package:felloapp/ui/pages/static/new_square_background.dart';
 import 'package:felloapp/ui/pages/userProfile/my_winnings/my_winnings_vm.dart';
 import 'package:felloapp/ui/service_elements/winners_prizes/prize_claim_card.dart';
@@ -17,11 +17,30 @@ import 'package:flutter/services.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 
-class MyWinningsView extends StatelessWidget {
+class MyWinningsView extends StatefulWidget {
   final openFirst;
   // final WinViewModel winModel;
 
   const MyWinningsView({this.openFirst = false, Key? key}) : super(key: key);
+
+  @override
+  State<MyWinningsView> createState() => _MyWinningsViewState();
+}
+
+class _MyWinningsViewState extends State<MyWinningsView>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +54,6 @@ class MyWinningsView extends StatelessWidget {
             statusBarColor: UiConstants.kTambolaMidTextColor,
           ),
           child: Scaffold(
-            extendBodyBehindAppBar: true,
             backgroundColor: UiConstants.kBackgroundColor,
             body: Stack(
               children: [
@@ -49,52 +67,88 @@ class MyWinningsView extends StatelessWidget {
 
                     return true;
                   },
-                  child: RefreshIndicator(
-                    backgroundColor: Colors.black,
-                    onRefresh: () async {
-                      model.init();
-                      await locator<UserService>().getUserFundWalletData();
-                      return Future.value(null);
-                    },
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: SizeConfig.pageHorizontalMargins,
-                          ),
-                          color: UiConstants.kTambolaMidTextColor,
-                          child: Row(
-                            children: [
-                              IconButton(
-                                onPressed: () {
-                                  AppState.backButtonDispatcher!.didPopRoute();
-                                },
-                                icon: const Icon(
-                                  Icons.arrow_back_ios,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              SizedBox(
-                                width: SizeConfig.padding20,
-                              ),
-                              Text(
+                  child: SafeArea(
+                    child: RefreshIndicator.adaptive(
+                      backgroundColor: Colors.black,
+                      onRefresh: () async {
+                        model.init();
+                        await locator<UserService>().getUserFundWalletData();
+                        return Future.value(null);
+                      },
+                      child: NestedScrollView(
+                        headerSliverBuilder: (context, value) {
+                          return <Widget>[
+                            SliverAppBar(
+                              backgroundColor: UiConstants.kTambolaMidTextColor,
+                              centerTitle: false,
+                              title: Text(
                                 "Scratch Cards",
                                 maxLines: 1,
                                 overflow: TextOverflow.clip,
-                                style:
-                                    TextStyles.title4.bold.colour(Colors.white),
+                                style: TextStyles.rajdhaniSB.title5
+                                    .colour(UiConstants.kTextColor),
                               ),
-                              const Spacer(),
-                              const FaqPill(
-                                type: FaqsType.rewards,
-                              )
-                            ],
-                          ),
-                        ),
-                        const PrizeClaimCard(),
-                        const ScratchCardsView(),
-                      ],
+                            ),
+                            const SliverToBoxAdapter(
+                              child: PrizeClaimCard(),
+                            ),
+                            SliverToBoxAdapter(
+                              child: SizedBox(height: SizeConfig.padding24),
+                            ),
+                            SliverAppBar(
+                              pinned: true,
+                              toolbarHeight: 0,
+                              bottom: ColoredTabBar(
+                                UiConstants.kBackgroundColor,
+                                TabBar(
+                                  controller: _tabController,
+                                  indicatorColor: Colors.white,
+                                  indicatorSize: TabBarIndicatorSize.label,
+                                  indicatorWeight: SizeConfig.padding4,
+                                  labelColor: Colors.white,
+                                  isScrollable: false,
+                                  onTap: (value) {
+                                    setState(() {});
+                                  },
+                                  tabs: [
+                                    Tab(
+                                        child: Text(
+                                      'Your Rewards',
+                                      style: TextStyles.sourceSansSB.body1,
+                                    )),
+                                    Tab(
+                                        child: Text(
+                                      'Earn Rewards',
+                                      style: TextStyles.sourceSansSB.body1,
+                                    )),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ];
+                        },
+                        body: Consumer<ScratchCardService>(
+                            builder: (context, model, child) {
+                          if (!model.isFetchingScratchCards &&
+                              model.allScratchCards.isEmpty &&
+                              _tabController.previousIndex == 0) {
+                            _tabController.animateTo(1,
+                                curve: Curves.easeInOut);
+                          }
+                          return model.isFetchingScratchCards &&
+                                  model.allScratchCards.isEmpty
+                              ? const Center(child: FullScreenLoader())
+                              : TabBarView(
+                                  controller: _tabController,
+                                  children: [
+                                    const ScratchCardsView(),
+                                    model.allScratchCards.isNotEmpty
+                                        ? const EarnRewardsDetails()
+                                        : const EarnRewardsIntro(),
+                                  ],
+                                );
+                        }),
+                      ),
                     ),
                   ),
                 ),
@@ -133,4 +187,21 @@ class MyWinningsView extends StatelessWidget {
       },
     );
   }
+}
+
+class ColoredTabBar extends Container implements PreferredSizeWidget {
+  ColoredTabBar(this.color, this.tabBar, {super.key});
+
+  @override
+  final Color color;
+  final TabBar tabBar;
+
+  @override
+  Size get preferredSize => tabBar.preferredSize;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: color,
+        child: tabBar,
+      );
 }
