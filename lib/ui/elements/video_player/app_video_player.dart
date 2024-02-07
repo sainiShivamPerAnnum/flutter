b34a2167e08f2538aa17b3eb1fd4735242ae8a9a
nishtha-fello <nishtha@fello.in>
2024-02-07 16:14:48 +0530

    caching videos logic

--- lib/ui/elements/video_player/app_video_player.dart
+++ lib/ui/elements/video_player/app_video_player.dart
@@ -1,8 +1,6 @@
-import 'package:felloapp/util/styles/size_config.dart';
-import 'package:felloapp/util/styles/ui_constants.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
-import 'package:video_player/video_player.dart';
+
+import '../../pages/hometabs/save/stories/story_view/story_view.dart';
 
 class AppVideoPlayer extends StatefulWidget {
   const AppVideoPlayer(this.link,
@@ -16,52 +14,11 @@ class AppVideoPlayer extends StatefulWidget {
 }
 
 class _AppVideoPlayerState extends State<AppVideoPlayer> {
-  VideoPlayerController? _controller;
-
-  @override
-  void initState() {
-    _controller = VideoPlayerController.network(widget.link)
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
-        _controller?.setLooping(true);
-        _controller?.play();
-      });
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return _controller?.value.isInitialized ?? false
-        ? ClipRRect(
-            borderRadius: BorderRadius.circular(SizeConfig.roundness12),
-            child: AspectRatio(
-              aspectRatio: widget.aspectRatio ?? _controller!.value.aspectRatio,
-              child: VideoPlayer(_controller!),
-            ),
-          )
-        : widget.showShimmer
-            ? Shimmer.fromColors(
-                baseColor: UiConstants.kUserRankBackgroundColor,
-                highlightColor: Colors.grey.shade800,
-                child: AspectRatio(
-                  aspectRatio: 1.4,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius:
-                          BorderRadius.circular(SizeConfig.roundness12),
-                    ),
-                  ),
-                ),
-              )
-            : Container();
+    return AppCachedVideo(
+        url: widget.link,
+        showShimmer: widget.showShimmer,
+        aspectRatio: widget.aspectRatio);
   }
 }
--- lib/ui/pages/hometabs/save/stories/story_view/widgets/story_video.dart
+++ lib/ui/pages/hometabs/save/stories/story_view/widgets/story_video.dart
@@ -6,6 +6,7 @@ import 'package:felloapp/util/locator.dart';
 import 'package:felloapp/util/styles/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:video_player/video_player.dart';
 
 import '../controller/story_controller.dart';
@@ -144,5 +145,131 @@ class StoryVideoState extends State<StoryVideo> {
     playerController?.dispose();
     _streamSubscription?.cancel();
     super.dispose();
+  }
+}
+
+class AppCachedVideo extends StatefulWidget {
+  AppCachedVideo(
+      {Key? key,
+      this.showShimmer = false,
+      this.aspectRatio,
+      this.requestHeaders,
+      required this.url})
+      : super(key: key ?? UniqueKey());
+
+  final bool showShimmer;
+  final double? aspectRatio;
+  final Map<String, dynamic>? requestHeaders;
+  final String url;
+
+  // static AppCachedVideo url(String url,
+  //     {required bool showShimmer,
+  //     double? aspectRatio,
+  //     Map<String, dynamic>? requestHeaders,
+  //     Key? key}) {
+  //   return AppCachedVideo(
+  //     VideoLoader(url, requestHeaders: requestHeaders),
+  //     key: key,
+  //     showShimmer: showShimmer,
+  //     aspectRatio: aspectRatio,
+  //   );
+  // }
+
+  @override
+  State<AppCachedVideo> createState() => _AppCachedVideoState();
+}
+
+class _AppCachedVideoState extends State<AppCachedVideo> {
+  late Stream<FileResponse> fileStream;
+
+  @override
+  void initState() {
+    super.initState();
+
+    fileStream = DefaultCacheManager().getFileStream(widget.url,
+        headers: widget.requestHeaders as Map<String, String>?,
+        withProgress: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: fileStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const SizedBox.shrink();
+        } else if (snapshot.hasData) {
+          final data = snapshot.data;
+
+          return (data is FileInfo)
+              ? _VideoPlayer(
+                  videoFile: data.file,
+                  aspectRatio: widget.aspectRatio,
+                )
+              : const _ShimmerWidget();
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
+
+class _VideoPlayer extends StatefulWidget {
+  const _VideoPlayer({super.key, required this.videoFile, this.aspectRatio});
+  final File videoFile;
+  final double? aspectRatio;
+  @override
+  State<_VideoPlayer> createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<_VideoPlayer> {
+  late final VideoPlayerController playerController;
+  @override
+  void initState() {
+    super.initState();
+    playerController = VideoPlayerController.file(widget.videoFile);
+    playerController.initialize().then((_) {
+      playerController.setLooping(true);
+      playerController.play();
+    });
+  }
+
+  @override
+  void dispose() {
+    playerController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(SizeConfig.roundness12),
+      child: AspectRatio(
+        aspectRatio: widget.aspectRatio ?? (playerController.value.aspectRatio),
+        child: VideoPlayer(playerController),
+      ),
+    );
+  }
+}
+
+class _ShimmerWidget extends StatelessWidget {
+  const _ShimmerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: UiConstants.kUserRankBackgroundColor,
+      highlightColor: Colors.grey.shade800,
+      child: AspectRatio(
+        aspectRatio: 1.4,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(SizeConfig.roundness12),
+          ),
+        ),
+      ),
+    );
   }
 }
